c2b750f7746bf1a36b4b14ee6e4d42b234e426b6
actiongamyoun <actiongamyoun@gmail.com>
2026-04-30 07:31:23 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Spots from './pages/Spots'
 import SpotDetail from './pages/SpotDetail'
 import Chat from './pages/Chat'
 import Transport from './pages/Transport'
+import RouteDetail from './pages/RouteDetail'
 import My from './pages/My'
 import AuthCallback from './pages/AuthCallback'
 
@@ -20,6 +21,7 @@ export default function App() {
             <Route path="/spots/:id" element={<SpotDetail />} />
             <Route path="/chat" element={<Chat />} />
             <Route path="/transport" element={<Transport />} />
+            <Route path="/transport/:slug" element={<RouteDetail />} />
             <Route path="/my" element={<My />} />
             <Route path="*" element={<Navigate to="/spots" replace />} />
           </Route>
